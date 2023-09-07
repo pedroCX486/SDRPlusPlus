0b860022631de66f1028c917e010847a74e9e99e
pedrocx486 <pedrocx486@gmail.com>
2023-09-07 03:24:51 -0300

    Fix forked drivers build path

--- rtlsdr_build.sh
+++ rtlsdr_build.sh
@@ -1,20 +1,14 @@
 #!/bin/bash
 
-make 
+echo "Building and installing RTL-SDR's fork of the Osmocom driver..."
 
 git clone https://github.com/rtlsdrblog/rtl-sdr-blog.git
-cd rtl-sdr/
+cd rtl-sdr-blog/
 mkdir build
 cd build/
-cmake ../
 
-if [ -z "$GITHUB_ENV" ]
-then
-    # If we're on Github Actions, then go for macOS11 libusb path.
-    make LIBRARY_PATH=/opt/homebrew/Cellar/libusb/1.0.26/lib/ 
-else
-    # If we're on desktop, then go for macOS13+ (M-series) libusb path.
-    make LIBRARY_PATH=/usr/local/Cellar/libusb/1.0.26/lib/
-fi
+cmake -DCMAKE_BUILD_TYPE=Release ../
+
+make -j3 LIBRARY_PATH=$(pkg-config --libs-only-L libusb-1.0 | sed 's/\-L//')
 
 sudo make install
